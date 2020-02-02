7082ae684a71acee0db02835fbc08b808d04cd6e
Justin Chi <jchi@memsql.com>
2020-02-01 22:34:47 -0800

    Update NOW

--- src/util.js
+++ src/util.js
@@ -14,7 +14,7 @@ const formatDateRange = (fromDate, toDate, parseFormat = "YYYY-MM-DD") => {
     return `${from} - ${to}`;
 };
 
-const NOW = moment("2020-01-30T23:59:59-05:00").format();
+const NOW = moment("2020-02-01T23:59:59-05:00").format();
 const ONE_WEEK_AGO = moment(NOW)
     .subtract(7, "d")
     .format();
